e6e3fa92fbeaa1de8d31ce1b0594c7376013ef0b
Gustavo-Gadelha <gustavofg2112@gmail.com>
2024-09-29 20:01:15 -0300

    Atualização na página de login

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -10,21 +10,28 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    TextEditingController _loginController = TextEditingController();
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
     var altura = MediaQuery.of(context).size.height;
     var largura = MediaQuery.of(context).size.width;
+    var alturaLogo = 0.20;
+    var alturaContainer = 0.07;
 
-    return SafeArea(
-      child: Material(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/logo.png', height: 150),
-            Padding(
-              padding: const EdgeInsets.only(top: 50, bottom: 25, left: 20, right: 20),
+    return Material(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/logo.png', height: altura * alturaLogo),
+          Padding(
+            padding: const EdgeInsets.only(top: 50, bottom: 25, left: 20, right: 20),
+            child: SizedBox(
+              height: altura * alturaContainer,
               child: TextField(
+                controller: _emailController,
                 cursorColor: const Color(0xFF1E90FF),
                 decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(20),
                   hintText: 'Email',
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
@@ -41,13 +48,17 @@ class _LoginViewState extends State<LoginView> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+            child: SizedBox(
+              height: altura * alturaContainer,
               child: TextField(
-                controller: _loginController,
+                controller: _passwordController,
                 cursorColor: const Color(0xFF1E90FF),
                 obscureText: true,
                 decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(20),
                   hintText: 'Senha',
                   suffixIcon: const Icon(Icons.visibility),
                   border: OutlineInputBorder(
@@ -65,29 +76,32 @@ class _LoginViewState extends State<LoginView> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Esqueci a senha",
-                      style: TextStyle(
-                        color: Color(0xFF757575),
-                      ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Esqueci a senha",
+                    style: TextStyle(
+                      color: Color(0xFF757575),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+            child: SizedBox(
+              height: altura * alturaContainer,
+              width: largura,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xFF1E90FF),
-                  minimumSize: Size(largura, altura * 0.06),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -103,8 +117,8 @@ class _LoginViewState extends State<LoginView> {
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
