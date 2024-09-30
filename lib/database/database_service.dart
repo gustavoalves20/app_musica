8a1d8b9a0d01eac89b6860bdf7df41e5e895460c
Gustavo-Gadelha <gustavofg2112@gmail.com>
2024-09-30 14:28:51 -0300

    Criação da classe que gerencia a instância da database sqlite

---
+++ lib/database/database_service.dart
@@ -0,0 +1,47 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseService {
+  static final DatabaseService _instance = DatabaseService._internal();
+  static Database? _database;
+
+  // Construtor interno privado
+  DatabaseService._internal();
+
+  // Construtor que retorna à instância privada ao invés de criar uma nova
+  factory DatabaseService() {
+    return _instance;
+  }
+
+  // Getter do atributo database, cria uma nova instância se _database for nula
+  // Reusa a instância criada após inicialização do banco
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initialize();
+    return _database!;
+  }
+
+  // Método para iniciar o banco e criar as tabelas
+  Future<Database> _initialize() async {
+    String path = join(await getDatabasesPath(), 'app.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  // Método privado executado  ao inicializar o banco
+  Future _onCreate(Database db, int version) async {
+    // Inicialização de cada tabela com respectivo CRUD
+  }
+
+  // Fecha a conexão com o banco
+  Future close() async {
+    final Database db = await database;
+    await db.close();
+    _database = null;
+  }
+}
